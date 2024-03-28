3dd9930458020ce002588b5fdc105a6a090a5419
fzaidi96 <f.zaidi182@gmail.com>
2024-03-28 22:35:16 +0000

    include new endpoints

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ import Database from "better-sqlite3";
 const db = new Database("database.db");
 
 
-app.get("/statuses", function (request, response) {
+app.get("https://fz-fb-server.onrender.com/", function (request, response) {
   const statuses = db.prepare("SELECT * FROM FBforGenz").all();
     response.json(statuses);
 });
 
 
 //adds a new status from user input into the database 
-app.post("/statuses", function (request, response) {
+app.post("https://fz-fb-server.onrender.com/statuses", function (request, response) {
   const username = request.body.username;
   const status = request.body.status;
   const newStatus = db.prepare(`INSERT INTO FBforGenz (username, status) VALUES (?,?)`).run(username, status);
@@ -27,7 +27,7 @@ app.post("/statuses", function (request, response) {
 });
 
 //deletes a status from the database 
-app.delete("/statuses/:id", function (request, response) {
+app.delete("https://fz-fb-server.onrender.com/statuses/:id", function (request, response) {
     const post = db.prepare("DELETE FROM FBforGenZ WHERE id=?").run(request.params.id);
     response.json(post);
 });
