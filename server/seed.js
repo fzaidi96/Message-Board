6df2719d6f1bd7da231d1e0144b56ce88ffa5356
fzaidi96 <f.zaidi182@gmail.com>
2024-03-30 22:15:14 +0000

    remove upvote

--- server/seed.js
+++ server/seed.js
@@ -5,21 +5,18 @@ db.exec(`
 CREATE TABLE IF NOT EXISTS FBforGenZ (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT,
-    status TEXT,
-    upvotes INTEGER DEFAULT 0
+    status TEXT
     )
 `);
 
 const insertStatus = db.prepare(
-  `INSERT INTO FBforGenZ (username, status, upvotes) VALUES (?, ?, ?)`
+  `INSERT INTO FBforGenZ (username, status) VALUES (?, ?)`
 );
 
-insertStatus.run("cottagecoregirlie", "mothering my tamagotchi fr",0);
-insertStatus.run("06techbro", "in my motorola rzr era",0);
-insertStatus.run("futureswiftie", "we need a global superstar country/pop diva on the scene no cap",0);
-insertStatus.run("markzuckerberg", "this website actually slaps tho",0);
-insertStatus.run("OGinfluencer", "felt cute, might delete later",0);
-insertStatus.run("bradpitt", "everyone loves me",0);
-insertStatus.run("jenniferaniston", "we can all see this Brad",0);
-
-
+insertStatus.run("cottagecoregirlie", "mothering my tamagotchi fr");
+insertStatus.run("06techbro", "in my motorola rzr era");
+insertStatus.run("futureswiftie", "we need a global superstar country/pop diva on the scene no cap");
+insertStatus.run("markzuckerberg", "this website actually slaps tho");
+insertStatus.run("OGinfluencer", "felt cute, might delete later");
+insertStatus.run("bradpitt", "everyone loves me");
+insertStatus.run("jenniferaniston", "we can all see this Brad");
